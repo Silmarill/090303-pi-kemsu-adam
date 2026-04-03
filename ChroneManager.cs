16d83a25878244fcc4a7c0ca4f496d1ce3dfc831
JetRex <v.bazanov7@gmail.com>
2026-04-03 09:01:53 +0700

    Add IChroneListener
    Добавил интерфейс из методички

--- ChroneManager.cs
+++ ChroneManager.cs
@@ -1,8 +1,23 @@
 ﻿using System;
 
-public class Class1
-{
-	public Class1()
-	{
-	}
+public interface IChroneListener {
+  void OnChroneTick();
+}
+
+public static class ChroneManager {
+  private List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+  public void AddListener(IChroneListener listener) {
+    _listenerList.Add(listener);
+  }
+
+  public void RemoveListerner(IChroneListener listener) {
+    _listenerList.Remove(listener);
+  }
+
+  public void MakeChrineTick() {
+    foreach (var listener in _listenerList) {
+      listener.OnCroneTick();
+    }
+  }
 }
