d7612f416848d72a49b20be540f363e5d3c30543
Konstantin <H1dra5@yandex.ru>
2026-04-03 09:01:32 +0700

    Перепечатал интерфейс и класс AsteroidEmitter

---
+++ Asteroidcs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Konovalov {
+
+  public interface IChroneListener {
+    void OnChroneTick();
+  }
+
+  public static class ChroneManager {
+    private static List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    public static void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    public static void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    public static void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChroneTick();
+      }
+    }
+  }
+
+  public class AsteroidEmitter {
+    private Queue<AsteroidEmitter> _available = new Queue<Asteroid>();
+      
+    public AsteroidEmitter(int initialSize) {
+      for (int i = 0; i < initialSize; ++i) {
+        AsteroidEmitter asteroid = new Asteroid();
+        _available.Enqueue(asteroid);
+      }
+    }
+
+    public AsteroidEmitter Spawn() {
+      if (_available.Count == 0) {
+        return new Asteroid();
+      }
+      return _available.Dequeue();
+    }
+
+    public void Recycle(AsteroidEmitter asteroid) {
+      asteroid.Reset();
+      _availavle.Enquenue(asteroid);
+    }
+  }
+
+  class void Asteroid
+
+}
