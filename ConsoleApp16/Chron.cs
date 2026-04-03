1302d4c1054d6469e95405e34923c76f3c07b5dd
komi1488 <pashaandreev66666@gmail.com>
2026-04-03 12:49:13 +0700

    комит

---
+++ ConsoleApp16/Chron.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids {
+  public interface IChroneListener {
+    void OnChoneTrick();
+  }
+  public class ChroneManager {
+    private List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    public void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    public void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    public void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChoneTrick();
+      }
+    }
+  }
+
+}
