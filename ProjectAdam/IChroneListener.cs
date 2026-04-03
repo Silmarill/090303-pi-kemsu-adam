1c30179d14399e90f47a3e91596187fde239f1fe
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-04-03 15:02:13 +0700

    Добавился IChroneListener & ChroneManager

---
+++ ProjectAdam/IChroneListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAdam {
+  public interface IChroneListener {
+    void OnChroneTick();
+  }
+
+  public static class ChroneManager {
+    private static List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    public static void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    public static void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    public static void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChroneTick();
+      }
+    }
+  }
+}
