9457f7897c9bcbc3e411a9e911611e35a0581071
gzlm163 <marusyagzl@gmail.com>
2026-04-03 13:17:18 +0700

    Added Asteroid and AsteroidState

---
+++ adam/Asteroid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Asteroid {
+  public int CurrentEchos;
+  public int MaxEchos;
+  public AsteroidState State;
+  public int SpawnID;
+  public int CreateID;
+
+  public Asteroid() {
+    Random random = new Random();
+    MaxEchos = random.Next(100, 1001);
+    CurrentEchos = MaxEchos;
+    State = AsteroidState.Idle;
+  }
+
+  public void Reset() {
+    CurrentEchos = MaxEchos;
+    State = AsteroidState.Idle;
+  }
+
+  public void OnChronTick() {
+    if (State == AsteroidState.Idle) {
+      CurrentEchos -= 100;
+      if (CurrentEchos <= 0) {
+        CurrentEchos = 0;
+        State = AsteroidState.Depleted;
+      }
+    }
+  }
+}
+
--- adam/AsteroidState.cs
+++ adam/AsteroidState.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-internal class Program {
-  static void Main(string[] args) {
-  }
+public enum AsteroidState {
+  Idle,
+  Depleted
 }
-
