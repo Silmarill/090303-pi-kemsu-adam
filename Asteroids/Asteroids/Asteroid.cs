1dfd988bb8391d2f21fc90ec6fed22b3d10d2816
RedTerror <anton-nekrasov07@mail.ru>
2026-04-03 08:59:11 +0700

    АМожетПростоГромСтать

---
+++ Asteroids/Asteroids/Asteroid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Asteroids {
+  internal class Asteroid {
+
+    public int MaxEchos = random.Next(100, 1000);
+    public int CurrentEchos;
+    public int SpawnID;
+    public int CreateID;
+
+    private static Random random = new Random();
+    public enum AsteroidState {
+      Idle = 1,
+      Depleted = 2
+    }
+    public AsteroidState State { get; set; }
+
+    public void Reset() {
+      CurrentEchos = MaxEchos;
+      State = AsteroidState.Idle;
+    }
+
+    public void OnChonTick() {
+      if (State == AsteroidState.Idle) {
+        CurrentEchos -= 100;
+        if (CurrentEchos < 0 || CurrentEchos == 0) {
+          CurrentEchos = 0;
+          State = AsteroidState.Depleted;
+        }
+      }
+    }
+  
+  }
+}
---
+++ Asteroids/Asteroids/IChroneListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Asteroids {
+  public interface IChroneListener {
+    void OnChronTick();
+  }
+
+  public static class ChroneManager {
+    private static List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    public static void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    public static void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    public static void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChronTick();
+      }
+    }
+    
+  }
+}
