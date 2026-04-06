1be71b6c1c06943060a9f5fd8a9accc5415f9a6f
komi1488 <pashaandreev66666@gmail.com>
2026-04-06 23:19:12 +0700

    сделал отдельный файл для ChroneManager

---
+++ ConsoleApp16/ChroneManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16 {
+  public static class ChroneManager {
+    private static List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    public static void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    public static void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    public static void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChoneTrick();
+      }
+    }
+  }
+}
--- ConsoleApp16/IChronListener.cs
+++ ConsoleApp16/IChronListener.cs
@@ -8,22 +8,4 @@ namespace ConsoleApp16 {
   public interface IChroneListener {
     void OnChoneTrick();
   }
-   
-  public static class ChroneManager {
-    private static List<IChroneListener> _listenerList = new List<IChroneListener>();
-
-    public static void AddListener(IChroneListener listener) {
-      _listenerList.Add(listener);
-    }
-
-    public static void RemoveListener(IChroneListener listener) {
-      _listenerList.Remove(listener);
-    }
-
-    public static void MakeChroneTick() {
-      foreach (var listener in _listenerList) {
-        listener.OnChoneTrick();
-      }
-    }
-  }
 }
