e57c95b8d1577b6ff790c94e500f55107ccf3183
gzlm163 <marusyagzl@gmail.com>
2026-04-06 17:21:21 +0700

    Add AsteroidEmitter, IChronoListener and ChronoManager

---
+++ adam/AsteroidEmitter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AsteroidEmitter {
+  private Queue<Asteroid> _available = new Queue<Asteroid>();
+  public static int _nextSpawnId = 1;
+
+  public AsteroidEmitter(int initialSize) {
+    for (int i = 0; i < initialSize; ++i) {
+      Asteroid asteroid = new Asteroid();
+      _available.Enqueue(asteroid);
+    }
+  }
+
+  public Asteroid Spawn() {
+    Asteroid asteroid;
+
+    if (_available.Count == 0) {
+      asteroid = new Asteroid();
+    } else {
+      asteroid = _available.Dequeue();
+    }
+
+    asteroid.SpawnID = _nextSpawnId++;
+    return asteroid;
+  }
+
+  public void Recycle(Asteroid asteroid) {
+    asteroid.Reset();
+    _available.Enqueue(asteroid);
+  }
+}
---
+++ adam/ChroneManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ChroneManager {
+  private List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+  public void AddListener(IChroneListener listener) {
+    _listenerList.Add(listener);
+  }
+
+  public void RemoveListener(IChroneListener listener) {
+    _listenerList.Remove(listener);
+  }
+
+  public void MakeChroneTick() {
+    foreach (var listener in _listenerList) {
+      listener.OnChroneTick();
+    }
+  }
+}
