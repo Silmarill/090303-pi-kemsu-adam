ee5581b598f27cf29b34d3cf7a957d3c3174d0ac
Kristina Shcherbakova <kristinagrom876@gmail.com>
2026-04-06 18:12:35 +0700

    change code

--- adam.sem2/Asteroid.cs
+++ adam.sem2/Asteroid.cs
@@ -5,74 +5,54 @@ namespace AsteroidSimulator.Models {
     public static int s_globalCreateCounter = 0;
     public static int s_globalSpawnCounter = 0;
 
-    public int _currentEchos;
-    public int _maxEchos;
-    public AsteroidState _state;
-    public int _createId;
-    public int _spawnId;
-
-    public int CurrentEchos => _currentEchos;
-    public int MaxEchos => _maxEchos;
-    public AsteroidState State => _state;
-    public int CreateId => _createId;
-    public int SpawnId => _spawnId;
+    public int CurrentEchos { get; private set; }
+    public int MaxEchos { get; private set; }
+    public AsteroidState State { get; private set; }
+    public int CreateId { get; private set; }
+    public int SpawnId { get; private set; }
 
     public Asteroid()
     {
       Random random = new Random();
-      int minEchos;
-      int maxEchos;
-      int echosLoss;
+      int minEchos = 100;
+      int maxEchos = 1000;
 
-      minEchos = 100;
-      maxEchos = 1000;
-      echosLoss = 100;
-
-      _maxEchos = random.Next(minEchos, maxEchos + 1);
-      _currentEchos = _maxEchos;
-      _state = AsteroidState.Idle;
-      _createId = ++s_globalCreateCounter;
-      _spawnId = 0;
+      MaxEchos = random.Next(minEchos, maxEchos);
+      CurrentEchos = MaxEchos;
+      State = AsteroidState.Idle;
+      CreateId = ++s_globalCreateCounter;
+      SpawnId = 0;
     }
 
     public void Reset()
     {
       Random random = new Random();
-      int minEchos;
-      int maxEchos;
-      int echosLoss;
+      int minEchos = 100;
+      int maxEchos = 1000;
 
-      minEchos = 100;
-      maxEchos = 1000;
-      echosLoss = 100;
-
-      _maxEchos = random.Next(minEchos, maxEchos + 1);
-      _currentEchos = _maxEchos;
-      _state = AsteroidState.Idle;
-      _spawnId = ++s_globalSpawnCounter;
+      MaxEchos = random.Next(minEchos, maxEchos);
+      CurrentEchos = MaxEchos;
+      State = AsteroidState.Idle;
+      SpawnId = ++s_globalSpawnCounter;
     }
 
     public void OnChronTick()
     {
-      int echosLoss;
-
-      echosLoss = 100;
-
-      if (_state == AsteroidState.Idle)
+      if (State == AsteroidState.Idle)
       {
-        _currentEchos -= echosLoss;
+        CurrentEchos -= 100;
 
-        if (_currentEchos <= 0)
+        if (CurrentEchos <= 0)
         {
-          _currentEchos = 0;
-          _state = AsteroidState.Depleted;
+          CurrentEchos = 0;
+          State = AsteroidState.Depleted;
         }
       }
     }
 
     public override string ToString()
     {
-      return $"Asteroid #{_spawnId} (Created: #{_createId}) | Echos: {_currentEchos}/{_maxEchos} | State: {_state}";
+      return $"Asteroid #{SpawnId} (Created: #{CreateId}) | Echos: {CurrentEchos}/{MaxEchos} | State: {State}";
     }
   }
 }
--- adam.sem2/Program.cs
+++ adam.sem2/Program.cs
@@ -6,20 +6,28 @@ using AsteroidSimulator.Managers;
 
 namespace AsteroidSimulator {
   class Program {
-    private static AsteroidEmitter s_asteroidEmitter;
-    private static List<Asteroid> s_activeAsteroids;
-    private static int s_chronCounter = 0;
-    private static Random s_random = new Random();
+    public static AsteroidEmitter s_asteroidEmitter;
+    public static List<Asteroid> s_activeAsteroids;
+    public static int s_chronCounter;
+    public static Random s_random = new Random();
 
     static void Main(string[] args)
     {
       Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-      int poolInitialSize = 5;
-      int startAsteroidsCount = 3;
-      int spawnInterval = 5;
-      int minNewAsteroids = 1;
-      int maxNewAsteroids = 3;
+      int poolInitialSize;
+      int startAsteroidsCount;
+      int spawnInterval;
+      int minNewAsteroids;
+      int maxNewAsteroids;
+
+      poolInitialSize = 5;
+      startAsteroidsCount = 3;
+      spawnInterval = 5;
+      minNewAsteroids = 1;
+      maxNewAsteroids = 3;
+
+      s_chronCounter = 0;
 
       s_asteroidEmitter = new AsteroidEmitter(poolInitialSize);
       s_activeAsteroids = new List<Asteroid>();
@@ -37,13 +45,12 @@ namespace AsteroidSimulator {
         ++s_chronCounter;
 
         Console.WriteLine($"CHRON #{s_chronCounter}");
-        Console.WriteLine("=========================================");
 
         ChronManager.MakeChronTick();
 
         if (s_chronCounter % spawnInterval == 0)
         {
-          int newAsteroidsCount = s_random.Next(minNewAsteroids, maxNewAsteroids + 1);
+          int newAsteroidsCount = s_random.Next(minNewAsteroids, maxNewAsteroids);
           Console.WriteLine($"Spawning {newAsteroidsCount} new asteroids!");
 
           for (int asteroidIndex = 0; asteroidIndex < newAsteroidsCount; ++asteroidIndex)
@@ -87,7 +94,6 @@ namespace AsteroidSimulator {
           }
         }
 
-        Console.WriteLine("=========================================");
         Console.WriteLine("Press Enter for next chron | ESC to exit");
 
         var key = Console.ReadKey(true);
