33f7be0654f58cd2652990c0d583b0de2c8a2388
Jameson54 <hehlol123123@mail.ru>
2026-04-06 20:23:56 +0700

    Доделано (наверное)

--- Adam/Models/Asteroid.cs
+++ Adam/Models/Asteroid.cs
@@ -5,8 +5,13 @@ namespace AsteroidSimulation.Models
 {
   public class Asteroid : IChronListener
   {
-    private static int globalCreateCounter = 0;
-    private static int globalSpawnCounter = 0;
+    public static int globalCreateCounter = 0;
+    public static int globalSpawnCounter = 0;
+    public static Random random = new Random();
+    public static int MaxEchosValue = 1000;
+    public static int echosLossPerTick = 100;
+    public static int rangeOffset = 1;
+
 
     public int CurrentEchos;
     public int MaxEchos;
@@ -14,13 +19,10 @@ namespace AsteroidSimulation.Models
     public int SpawnID;
     public int CreateID;
 
-    private const int echosLossPerTick = 100;
-    private static readonly Random random = new Random();
-
     public Asteroid()
     {
       CreateID = ++globalCreateCounter;
-      MaxEchos = random.Next(100, 1001);
+      MaxEchos = random.Next(echosLossPerTick, MaxEchosValue + rangeOffset);
       Reset();
     }
 
