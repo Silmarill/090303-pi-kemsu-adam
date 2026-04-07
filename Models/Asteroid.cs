e349f0c97f5f2e0ad893052aa61bb1852c0b74d7
6lood1ess <6lo.od1ess@gmail.com>
2026-04-07 17:47:37 +0700

    Added Asteroid class with fields and methods

---
+++ Models/Asteroid.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AsteroidZoneSimulation.Core;
+
+namespace AsteroidZoneSimulation.Models {
+  public class Asteroid : IChroneListener {
+    int globalCreateCounter = 0;
+    int losingEchos = 100;
+
+    public int CurrentEchos;
+    public int MaxEchos;
+    public AsteroidState State;
+    public int SpawnID;
+    public int CreateID;
+
+    public Random random = new Random();
+
+    public Asteroid() {
+      MaxEchos = random.Next(100, 1001);
+      CurrentEchos = MaxEchos;
+      State = AsteroidState.Idle;
+      CreateID = ++globalCreateCounter;
+      SpawnID = 0;
+    }
+
+    public void Reset() {
+      CurrentEchos = MaxEchos;
+      State = AsteroidState.Idle;
+    }
+
+    public void OnChroneTick() {
+      if (State == AsteroidState.Idle) {
+        CurrentEchos -= losingEchos;
+
+        if (CurrentEchos <= 0) {
+          CurrentEchos = 0;
+          State = AsteroidState.Depleted;
+        }
+      }
+    }
+
+    public void SetSpawnID(int id) {
+      SpawnID = id;
+    }
+
+    public string GetAsteroidInformation() {
+      return $"Create ID:{CreateID} | Spawn ID:{SpawnID} | Echoes:{CurrentEchos}/{MaxEchos} | State:{State}";
+    }
+  }
+}
