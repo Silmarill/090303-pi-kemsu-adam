b62d442a816d3bdc3685f10320c1f09afce73166
neldori <nexusstail@gmail.com>
2026-04-07 23:48:47 +0700

    реализовал ChroneManager и переместил в папку

---
+++ AsteroidSimulation/AsteroidSimulation/Chrone/ChroneManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidSimulation {
+  public static class ChroneManager {
+    private static List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    static public void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    static public void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    static public void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChronTick();
+      }
+    }
+  }
+} 
