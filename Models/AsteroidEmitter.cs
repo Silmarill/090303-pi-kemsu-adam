1eca03676e5c5914e2268cc1861a8650910b540a
6lood1ess <6lo.od1ess@gmail.com>
2026-04-07 18:29:09 +0700

    Implemented the AsteroidEmitter class

---
+++ Models/AsteroidEmitter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace AsteroidZoneSimulation.Models {
+  public class AsteroidEmitter {
+    private Queue<Asteroid> _available = new Queue<Asteroid>();
+
+    public AsteroidEmitter(int initialSize) {
+      for (int asteroidCount = 0; asteroidCount < initialSize; ++asteroidCount) {
+        Asteroid asteroid = new Asteroid();
+        _available.Enqueue(asteroid);
+      }
+    }
+
+    public Asteroid Spawn() {
+      if (_available.Count == 0) {
+        return new Asteroid();
+      }
+
+      return _available.Dequeue();
+    }
+
+    public void Recycle(Asteroid asteroid) {
+      asteroid.Reset();
+      _available.Enqueue(asteroid);
+    }
+
+    public int GetAvailableCount() {
+      return _available.Count;
+    }
+  }
+}
