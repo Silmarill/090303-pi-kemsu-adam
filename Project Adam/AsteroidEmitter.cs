a699da87a4e0a3d861154d22bbd155d6716bad9b
dezue <mistfoll@mail.ru>
2026-04-07 18:48:30 +0700

    asteroidemitter

--- Project Adam/AsteroidEmitter.cs
+++ Project Adam/AsteroidEmitter.cs
@@ -1,23 +1,35 @@
-﻿namespace Project_Adam {
+﻿using System.Collections.Generic;
+
+namespace Project_Adam {
   public class AsteroidEmitter {
     private Queue<Asteroid> _available = new Queue<Asteroid>();
+    private int _totalSpawnedCount = 0;
 
     public AsteroidEmitter(int initialSize) {
-      for (int i = 0; i < initialSize; ++i) {
-        AsteroidEmitter asteroid = new Asteroid();
-        _available.Enqueue(asteroid);
+      for (int asteroidIndex = 0; asteroidIndex < initialSize; ++asteroidIndex) {
+        Asteroid newAsteroid = new Asteroid();
+        _available.Enqueue(newAsteroid);
       }
+    }
 
-    public Asteroid spawn() {
+    public Asteroid Spawn() {
+      Asteroid spawnedAsteroid;
+
       if (_available.Count == 0) {
-        return new Asteroid();
+        spawnedAsteroid = new Asteroid();
+      } else {
+        spawnedAsteroid = _available.Dequeue();
       }
-      return _available.Dequeue();
+
+      spawnedAsteroid.SpawnID = ++_totalSpawnedCount;
+
+      return spawnedAsteroid;
     }
 
-    public void Recycle(Asteroid asteroid) {
-      asteroid.Reset();
-      _available.Enqueue(asteroid);
+    public void Recycle(Asteroid asteroidToRecycle) {
+      asteroidToRecycle.Reset();
+      _available.Enqueue(asteroidToRecycle);
     }
- }
+  }
+}
 
