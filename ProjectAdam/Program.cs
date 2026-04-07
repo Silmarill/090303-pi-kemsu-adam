cbe5c1d0ffd87df21c4277dbf358871f32017bbd
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-04-07 20:07:06 +0700

    Фиксы (нормальные, не в main 😭)

--- ProjectAdam/Program.cs
+++ ProjectAdam/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectAdam
-{
-    internal class Program
-    {
-        static void Main(string[] args) {
-         
-        }
+namespace ProjectAdam {
+  internal class Program {
+    static void Main(string[] args) {
+      AsteroidEmitter emitter = new AsteroidEmitter(5);
+      List<Asteroid> activeAsteroids = new List<Asteroid>();
+
+      int chroneCount = 0;
+
+      for (int i = 0; i < 3; i++) {
+        Asteroid asteroid = emitter.Spawn();
+        activeAsteroids.Add(asteroid);
+        ChroneManager.AddListener(asteroid);
+      }
     }
+  }
 }
