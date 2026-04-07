c33f14e9c8d97a623d718d85366a407300a3a566
6lood1ess <6lo.od1ess@gmail.com>
2026-04-07 17:19:40 +0700

    Observer added to ChroneManager.cs

---
+++ Core/ChroneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AsteroidZoneSimulation.Core {
+  public static class ChroneManager {
+    private List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+    public void AddListener(IChroneListener listener) {
+      _listenerList.Add(listener);
+    }
+
+    public void RemoveListener(IChroneListener listener) {
+      _listenerList.Remove(listener);
+    }
+
+    public void MakeChroneTick() {
+      foreach (var listener in _listenerList) {
+        listener.OnChroneTick();
+      }
+    }
+  }
+}
