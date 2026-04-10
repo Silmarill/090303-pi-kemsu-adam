0068b436271f9c8be4dbcbf2ff90ba09eceed4e4
komi1488 <pashaandreev66666@gmail.com>
2026-04-10 12:58:29 +0700

    комит пуш

---
+++ ConsoleApp16/MotherShip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16 {
+  internal class MotherShip {
+    public int ID;
+    public string Name;
+    public int AsteroidMined;
+    public int BiteSize;
+
+
+  }
+}
