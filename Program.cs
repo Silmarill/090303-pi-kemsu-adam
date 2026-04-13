c118f67d3a0195fa2d1598a58186c7c4a1d804b1
6lood1ess <6lo.od1ess@gmail.com>
2026-04-13 17:53:24 +0700

    Program.cs updated

--- Program.cs
+++ Program.cs
@@ -1,37 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using AsteroidZoneSimulation.Models;
 using AsteroidZoneSimulation.Core;
 
 namespace AsteroidZoneSimulation {
   class Program {
 
-    public static List<Asteroid> activeAsteroids = new List<Asteroid>();
-    public static AsteroidEmitter emitter;
+    private static MotherShip _motherShip;
+    private static AsteroidEmitter _asteroidEmitter;
     public static int chroneCounter = 0;
-    public static Random random = new Random();
 
     static void Main() {
       Console.Title = "Проект Адам. Астероидная зона";
 
+      _motherShip = new MotherShip(5, 500, 50);
+       
+      //Пул 5, спавн каждые 5 хронов, от 1 до 3 астероидов
+      _asteroidEmitter = new AsteroidEmitter(5, _motherShip, 5, 1, 3);
+      _motherShip.SetEmitter(_asteroidEmitter);
+
       int initialNumberOfAsteroids = 3;
         
-      emitter = new AsteroidEmitter(5);
-        
       for (int spawnIndex = 0; spawnIndex < initialNumberOfAsteroids; ++spawnIndex) {
-        SpawnNewAsteroid();
+        Asteroid asteroid = _asteroidEmitter.Spawn();
+        _motherShip.AddAsteroid(asteroid);
       }
-        
-      Console.WriteLine($"\n[Старт] Обнаружено активных астероидов: {activeAsteroids.Count}");
-      DisplayAsteroidsInfo();
+
+      _motherShip.AssignIdleHarvesters();
         
       while (true) {
         Console.WriteLine("\n=======================================" +
                           $"\n        ░▒▓█ХРОН #{chroneCounter}█▓▒░" +
-                          "\n=======================================" +
-                          "\n\nНажмите ENTER для следующего хрона или ESC для выхода");
+                          "\n=======================================");
+
+        _motherShip.PrintAsteroidsInfo();
+        _motherShip.PrintHarvestersInfo();
+        _motherShip.PrintTotalMined();
             
+        Console.WriteLine($"\n[Пул] Свободных астероидов: {_asteroidEmitter.GetAvailableCount()}" +
+                          "\n\n----------------------------------------------------------------" +
+                          "\n  ENTER - следующий хрон | R - суммарная добыча | ESC - выход" +
+                          "\n----------------------------------------------------------------");
+
         ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             
         if (keyInfo.Key == ConsoleKey.Escape) {
@@ -39,6 +51,10 @@ namespace AsteroidZoneSimulation {
           break;
         } else if (keyInfo.Key == ConsoleKey.Enter) {
           ProcessChrone();
+        } else if (keyInfo.Key == ConsoleKey.R) {
+          _motherShip.PrintTotalMined();
+          Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+          Console.ReadKey(true);
         } else {
           Console.WriteLine("Введена неверная клавиша! Используйте ENTER или ESC");
         }
@@ -53,51 +69,15 @@ namespace AsteroidZoneSimulation {
                         $"\n        ░▒▓█ХРОН #{chroneCounter}█▓▒░" +
                         "\n=======================================");
         
-      foreach (var asteroid in activeAsteroids) {
-        asteroid.OnChroneTick();
+      ChroneManager.MakeChroneTick();
+
+      _motherShip.AssignIdleHarvesters();
+        
+      if (chroneCounter % 15 == 0) {
+        _motherShip.PrintFullWorklog();
       }
-        
-      if (chroneCounter % 5 == 0) {
-        int newAsteroidsCount = random.Next(1, 4);
-        Console.WriteLine($"\nОбнаружено {newAsteroidsCount} новых астероидов!\n");
-            
-        for (int spawnIndex = 0; spawnIndex < newAsteroidsCount; ++spawnIndex) {
-          SpawnNewAsteroid();
-        }
-      }
-        
-      List<Asteroid> depletedAsteroids = activeAsteroids.Where(asteroid => asteroid.State == AsteroidState.Depleted).ToList();
-        
-      if (depletedAsteroids.Any()) {
-        Console.WriteLine($"\nНайдено {depletedAsteroids.Count} истощённых астероидов");
-        foreach (var asteroid in depletedAsteroids) {
-          activeAsteroids.Remove(asteroid);
-          emitter.Recycle(asteroid);
-        }
-      }
-        
-      Console.WriteLine($"\n.* АКТИВНЫЕ АСТЕРОИДЫ ({activeAsteroids.Count}) *.\n");
-      DisplayAsteroidsInfo();
-        
-      Console.WriteLine($"\nСвободных астероидов в пуле: {emitter.GetAvailableCount()}" +
-                        $"\nВсего спавнов: {activeAsteroids.Max(asteroid => asteroid.SpawnID)}");
-    }
-    
-    static void SpawnNewAsteroid() {
-      Asteroid asteroid = emitter.Spawn();
-      activeAsteroids.Add(asteroid);
-      Console.WriteLine($"[Спавн] Астероид {asteroid} появился!");
-    }
-    
-    static void DisplayAsteroidsInfo() {
-      if (activeAsteroids.Count == 0) {
-        Console.WriteLine("Нет активных астероидов. Нашей жизни ничего не угрожает");
-        return;
-      }
-        
-      for (int activeAsteroidIndex = 0; activeAsteroidIndex < activeAsteroids.Count; ++activeAsteroidIndex) {
-        Console.WriteLine($"{activeAsteroidIndex + 1}. {activeAsteroids[activeAsteroidIndex].GetAsteroidInformation()}");
-      }
+
+      Thread.Sleep(30);
     }
   }
 }
