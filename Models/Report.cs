e685595df63de39d8ee1658deeb010ccadcce965
6lood1ess <6lo.od1ess@gmail.com>
2026-04-13 14:46:07 +0700

    Created a Report class in which the report of the completed extraction of one asteroid will be compiled and displayed on the console

---
+++ Models/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AsteroidZoneSimulation.Models {
+  public class Report {
+    public int JobNumber;
+    public int AsteroidSpawnID;
+    public int AmountMined;
+
+    public Report(int jobNumber, int asteroidSpawnID, int amountMined) {
+      JobNumber = jobNumber;
+      AsteroidSpawnID = asteroidSpawnID;
+      AmountMined = amountMined;
+    }
+
+    public void Print() {
+      Console.WriteLine($"    Задание #{JobNumber} | Астероид (SpawnID): {AsteroidSpawnID} | Добыто: {AmountMined} ед.");
+    }
+  }
+}
