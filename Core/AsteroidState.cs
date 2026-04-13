b6085eeee83711d49b3e9836f1c799b0d191f41d
v01d <Kevin_Vearez@mail.ru>
2026-04-13 15:01:48 +0700

    New enums, interfaces, and base classes

--- Core/AsteroidState.cs
+++ Core/AsteroidState.cs
@@ -1,5 +1,6 @@
 ﻿// Определяет возможные состояния, в которых может находиться астероид
 public enum AsteroidState {
   Idle,
+  Mining,
   Depleted
 }
---
+++ Entities/Report.cs
@@ -0,0 +1,17 @@
+﻿public class Report {
+  public int JobNumber { get; set; }
+  public int AsteroidSpawnID { get; set; }
+  public int AmountMined { get; set; }
+
+  // Конструктор для создания отчета
+  public Report(int jobNumber, int asteroidSpawnID, int amountMined) {
+    JobNumber = jobNumber;
+    AsteroidSpawnID = asteroidSpawnID;
+    AmountMined = amountMined;
+  }
+
+  // Перегрузка метода ToString для удобного отображения информации о отчете
+  public override string ToString() {
+    return $"Job #{JobNumber} | Asteroid SpawnID: {AsteroidSpawnID} | Mined: {AmountMined}";
+  }
+}
