66c7fb361ffbb5ba1b4e9df03bcc8a619ba69b32
Konstantin <H1dra5@yandex.ru>
2026-04-14 19:00:18 +0700

    Доработал класс MotherShip

--- Pattern Observer and Object pool, Laba 5/MotherShipRealisation/MotherShip.cs
+++ Pattern Observer and Object pool, Laba 5/MotherShipRealisation/MotherShip.cs
@@ -4,11 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Pattern_Observer_and_Object_pool__Laba_5.HarvesterShipRealisation;
+using Pattern_Observer_and_Object_pool__Laba_5.PoolObject;
 using Pattern_Observer_and_Object_pool__Laba_5.ProgramReport;
 
 namespace Pattern_Observer_and_Object_pool__Laba_5 {
   public class MotherShip {
     public List<HarvesterShip> Fleet;
-    Dictionary<string, List<Report>>
+    public Dictionary<string, List<Report>> workLog;
+    List<Asteroid> _activeAsteroids;
+    AsteroidEmitter _asteroidEmitter;
+
+    public void AddAsteroid(Asteroid asteroid) {
+      _activeAsteroids.Add(asteroid);
+    }
+
+    public void RemoveAsteroid(Asteroid asteroid) {
+      _activeAsteroids.Remove(asteroid);
+    }
+
+
+    public MotherShip(int harvesterCount, int cargoCapacity, int biteSize) {
+    }
+
+    public Asteroid GetIdleAsteroid() {
+      for (int count = 0; count < _activeAsteroids.Count; ++count) {
+        if (_activeAsteroids[count].State == Asteroid.AsteroidState.Idle) {
+          return _activeAsteroids[count];
+        }
+      }
+      return null;
+    }
+
+    public void AssignIdleHarvesters(Asteroid asteroid) {
+      if (asteroid.State == Asteroid.AsteroidState.Idle) {
+        for (int count = 0; count < Fleet.Count; ++count) {
+          
+        }
+      }
+    }
+
+    public void FinishHarvest(HarvesterShip harvester) {
+      for (int count = 0; count < Fleet.Count; ++count) {
+        Fleet[count].state = HarvesterState.Idle;
+      }
+    }
   }
 }
